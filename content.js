bdcb997d8f35fb99e166ce7d177701cdc9c0124a
thilaksundaram2 <thilak@structlabs.io>
2023-10-06 16:47:26 +0800

    Added HTML + Code Refactor + Fixed Summarise Issue

--- content.js
+++ content.js
@@ -1,3 +1,38 @@
+  const style = document.createElement('style');
+  style.textContent = `
+    .summies_yt_summary_btn,
+    .summies_yt_summary_btn_white {
+      border-radius: 50px;
+      border:none;
+      font-size: 12px;
+      line-height: 17px;
+      color: #FFFFFF;
+      padding: 10px 15px;
+      font-weight: bold;
+      letter-spacing:0.75px;
+    }
+    .summies_yt_summary_btn {
+      background: linear-gradient(102.34deg, #4658FF 5.72%, #49B9D9 113.86%);
+      color: #FFFFFF;
+    }
+    .summies_yt_summary_btn_white {
+      background: #fff;
+      color: rgba(33, 33, 33, 0.65);
+    }
+    .summies_yt_summary_btn:hover,
+    .summies_yt_summary_btn_white:hover {
+      cursor: pointer;
+    }
+    .summies_yt_container {
+      background: rgb(255, 255, 255, 0.17);
+      border-radius: 10px;
+      width:100%;
+      padding: 10px 10px;
+      display:flex;
+      justify-content: space-between;
+    }
+  `;
+  document.head.appendChild(style);
   function createButton() {
     const button = document.createElement('button');
     button.textContent = 'Summarise Video';
@@ -5,19 +40,40 @@
     button.addEventListener('click', () => { summariseContent() });
     return button;
   }
+  function createHTML() {
+    //DIV CONTAINER
+    const div = document.createElement('div');
+    div.id = 'my-div';
+    div.className = 'summies_yt_container';
+    
+    //SUMMIES LOGO
+    const img = document.createElement('img');
+    img.src = 'https://cdn-images-1.medium.com/v2/resize:fit:1280/1*jYKuDOUj7w8ABAPYLqd0CQ.png';
+    img.width = 35;
+    div.appendChild(img);
+    
+    //SUMMARISE BUTTON
+    const button = document.createElement('button');
+    button.className = 'summies_yt_summary_btn_white';
+    button.textContent = 'Summarise this content';
+    button.addEventListener('click', summariseContent);
+    div.appendChild(button);
+
+    return div;
+  }
   function injectButton() {
     setTimeout(() => {
         const targetDiv = document.querySelector('ytd-watch-flexy #secondary');
         if (targetDiv && "www.youtube.com" === window.location.hostname) {
-            const buttonElement = createButton();
+            // const buttonElement = createButton();
+            const buttonElement = createHTML();
             targetDiv.insertBefore(buttonElement, targetDiv.firstChild);
         } else if ("chat.openai.com" === window.location.hostname) {
             pasteIntoTextarea()
         }
-    }, 500);
+    }, 200);
   }
   async function fetchTranscript() {
- 
     // 1. FETCH VIDEO_ID VIA URL
     const videoId = new URL(window.location.href).searchParams.get('v');
 
@@ -29,12 +85,9 @@
 
     if (r.length < 2) return;
     if (i.length < 2) return;
-    const a = JSON.parse(r[1].split(',"videoDetails')[0].replace("\n", "")).playerCaptionsTracklistRenderer.captionTracks,
-      o = Array.from(a).map((e) => e.name.simpleText),
-      s = i[1].split('","lengthSeconds"')[0] ?? "",
-      l = "English";
+    const a = JSON.parse(r[1].split(',"videoDetails')[0].replace("\n", "")).playerCaptionsTracklistRenderer.captionTracks;
 
-    // 2.1 RAW TRANSCRIPT
+    // RAW TRANSCRIPT
     // console.log(`👇 VIDEO TRANSCRIPT RAW 👇\n ${a[0].baseUrl}`)
 
     // XML TO TEXT
@@ -83,7 +136,6 @@
     const textFromClipboard = await navigator.clipboard.readText();
     const textToPopulate = textFromClipboard.replace(/&#39;/g, "'") || 'default content';
 
-    setTimeout(() => {
       if (textarea) {
         textarea.textContent = textToPopulate;
         textarea.focus();
@@ -94,7 +146,6 @@
       } else {
         console.error('Textarea element not found.');
       }
-    }, 500);
   }
   function getYouTubeVideoId(url) {
     const urlParams = new URLSearchParams(new URL(url).search);
@@ -111,6 +162,7 @@
     // STEP 4 OPEN CHATGPT IN A NEW TAB ✅
     chrome.runtime.sendMessage({ type: 'OPEN_NEW_TAB' });
   }
+
   window.addEventListener('load', () => {
     // STEP 1: INJECT BUTTON IN YOUTUBE ON PAGE LOAD
     injectButton();
